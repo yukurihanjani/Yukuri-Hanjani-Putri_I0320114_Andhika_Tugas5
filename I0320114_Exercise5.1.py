bc75353f4add570acc778dfb264335f919b3e17b
Yukuri Hanjani <hanjaniyukuri@gmail.com>
2021-04-01 08:10:01 +0700

    Exercise

---
+++ I0320114_Exercise5.1.py
@@ -0,0 +1,7 @@
+# penggunaan if untuk satu kasus
+# inputkan bilangan
+bilangan = int(input("Masukkan bilangan bulat: ")
+# memeriksa bilangan
+if bilangan % 2 == 0
+    print("%d adalah bilangan genap" % bilangan)
+print()
